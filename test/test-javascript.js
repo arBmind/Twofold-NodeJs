29ab09970a877324d49e4b2c8c48d6c3452b7642
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2014-11-15 19:28:39 +0100

    javascript tests work * split source code

--- test/test-javascript.js
+++ test/test-javascript.js
@@ -1,6 +1,7 @@
 var expect = require("should");
 var data_driven = require('data-driven');
-var template_engine = require("../lib/template-engine.js");
+var PreparedTemplateBuilder = require('../lib/PreparedTemplateBuilder.js');
+var LoaderStatus = require('../lib/TextLoader.js').Status;
 var util = require('util');
 
 function FakeMessageHandler() {
@@ -24,7 +25,7 @@ function FakeTextLoader(text) {
   return {
     load: function(name) {
       return {
-        status: template_engine.Loader.Success,
+        status: LoaderStatus.Success,
         fullPath: name,
         text: text
       };
@@ -59,7 +60,7 @@ describe("Interpolation", function() {
       {name: "escaped expr 2", template: "|###{test}", js: indentPart("", 0) + output("#", 1) + expression("test", 2) + newLine},
       {name: "string expr 1", template: "|#{\"}\"}", js: indentPart("", 0) + expression("\"}\"", 1) + newLine},
       {name: "string expr 2", template: "|#{'\\'\\\"}'}", js: indentPart("", 0) + expression("'\\'\\\"}'", 1) + newLine},
-      {name: "nested expr", template: "|#{'#{hello}'}", js: indentPart("", 0) + expression("'#{hello}'", 1) + newLine},
+      {name: "nested expr", template: "|#{'#{hello}'}", js: indentPart("", 0) + expression("'#{hello}'", 1) + newLine}
     ];
   }
 
@@ -69,7 +70,7 @@ describe("Interpolation", function() {
       var javaScriptText = ctx.js;
       var messageHandler = FakeMessageHandler();
       var textLoader = FakeTextLoader(templateText);
-      var builder = template_engine.PreparedTemplateBuilder(messageHandler, textLoader);
+      var builder = PreparedTemplateBuilder(messageHandler, textLoader);
       var prepared = builder.build(FAKE_TEMPLATE_NAME);
 
       prepared.should.have.property("javascript");
@@ -80,6 +81,67 @@ describe("Interpolation", function() {
       messageHandler.count.javaScriptMessage.should.equal(0);
 
       prepared.javascript.should.equal(javaScriptText, "prepared.javascript");
+    });
+  });
+});
+
+describe("SourceMap", function() {
+  function buildTestData() {
+    return [
+      //                                                1         2
+      //                                       123456789012345678901234567
+      {name: "inline expression 1", template: "if(test) { return 1 : 2 }", search: "1 : 2", position: [1, 19]},
+      {name: "inline expression 2", template: "| simple #{(test)? 1 : 2 )}", search: "1 : 2", position: [1, 20]},
+
+      //                                                 1         2
+      //                                       123456789 012345678901
+      {name: "inline expression 3", template: "| simple \n|#{'hello'}", search: "hello", position: [2, 5]},
+
+      //                                              1         2
+      //                                    123456789 012345678901
+      {name: "generated code 1", template: "| simple \n|#{'hello'}", search: "pend('hello", position: [2, 2]},
+      {name: "generated code 2", template: "|#{'hello'}", search: ", 1);_template.pop", position: [1, 11]}
+    ];
+  }
+  function count(string, subString, maxIdx) {
+    if (subString.length <= 0)
+      return string.length+1;
+
+    var n = 0, pos = 0;
+    var step = subString.length;
+    while (true) {
+      pos = string.indexOf(subString, pos);
+      if (pos < 0) break; // not found
+      if (pos > maxIdx) break; // end reached
+      n++;
+      pos += step;
+    }
+    return n;
+  }
+
+  data_driven(buildTestData(), function() {
+    it("#{name}", function(ctx) {
+      var templateText = ctx.template,
+        searchText = ctx.search,
+        templateLine = ctx.position[0],
+        templateColumn = ctx.position[1];
+
+        var messageHandler = FakeMessageHandler();
+        var textLoader = FakeTextLoader(templateText);
+        var builder = PreparedTemplateBuilder(messageHandler, textLoader);
+        var prepared = builder.build(FAKE_TEMPLATE_NAME);
+
+        var js = prepared.javascript;
+        var searchIdx = js.lastIndexOf(searchText);
+        searchIdx.should.not.equal(-1);
+        var lastNewline = js.lastIndexOf("\n", searchIdx) + 1;
+        var generatedColumn = (searchIdx - lastNewline) + 1;
+        var generatedLine = count(js, "\n", lastNewline) + 1;
+
+        var source = prepared.sourceMap.findPosition(generatedLine, generatedColumn);
+
+        source.line.should.equal(templateLine);
+        source.column.should.equal(templateColumn);
     });
   });
 });
